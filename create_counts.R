fe72c004c8ec755f5ca0ab9a771e7c01509e4489
abhivij <abhishek.kcvijayan@gmail.com>
2021-07-30 13:52:21 +1000

    mirna counts

--- create_counts.R
+++ create_counts.R
@@ -1,67 +1,52 @@
 library(tidyverse)
-
-library(TxDb.Hsapiens.UCSC.hg38.knownGene)
-txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
-k <- keys(txdb, keytype = "TXNAME")
-tx2gene <- AnnotationDbi::select(txdb, k, "GENEID", "TXNAME") 
-
-tx2gene <- tx2gene %>%
-  drop_na("GENEID")
-
-
 library(tximport)
-setwd("/home/abhivij/UNSW/VafaeeLab/GBMPlasmaEV")
+library(biomaRt)
 
-files <- list.files("quants_subset", recursive = TRUE, pattern = "*.sf")
-samples <- sapply(list.files("quants_subset"), FUN = 
+base_dir <- "/home/abhivij/UNSW/VafaeeLab/GBMPlasmaEV"
+quant_dir_name <- "quants_subset"
+
+setwd(base_dir)
+
+files <- list.files(quant_dir_name, recursive = TRUE, pattern = "*.sf")
+samples <- sapply(list.files(quant_dir_name), FUN = 
                    function(x){
                      return(strsplit(x, fixed = TRUE, split = "_")[[1]][1])
                    }) 
 names(files) <- samples
 
-setwd("/home/abhivij/UNSW/VafaeeLab/GBMPlasmaEV/quants_subset")
-all(file.exists(files))
+quant_dir <- paste(base_dir, quant_dir_name, sep = "/")
+setwd(quant_dir)
 
-
-txi <- tximport(files, type = "salmon", tx2gene = tx2gene)
-names(txi)
-
-counts <- txi$counts
-
-counts <- as.data.frame(counts) %>%
-  filter(rowSums(counts) > 0)
-
-
-write.table(x = counts, file = "counts_subset.txt", quote = FALSE, sep = "\t", row.names=TRUE, col.names=NA)
+print(paste("all file exists ?", all(file.exists(files))))
 
 
 txi <- tximport(files, type = "salmon", txOut = TRUE)
-names(txi)
-
 counts <- txi$counts
-
 counts <- as.data.frame(counts) %>%
   filter(rowSums(counts) > 0)
 
-rnames <- data.frame(rownames(counts))
-colnames(rnames) <- "rnames"
-  
-rnames <- rnames %>%
-  separate("rnames", c("n", NA))
-
-rnames <- sapply(rownames(counts), FUN = 
-                    function(x){
-                      return(strsplit(x, fixed = TRUE, split = ".")[[1]][1])
-                    }) 
 
 
-write.table(x = counts, file = "transcript_counts_subset.txt", quote = FALSE, sep = "\t", row.names=TRUE, col.names=NA)
-
-
-library(biomaRt)
-
-
+#get miRNA-ensemblid mapping from ensembl biomart
 ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl")
-miRNA <- getBM( attributes=c("mirbase_id","ensembl_transcript_id"),
-                filters=c("with_mirbase"),values=list(TRUE), mart=ensembl)
+miRNA <- getBM(attributes=c("mirbase_id","ensembl_transcript_id"),
+              filters=c("with_mirbase"),values=list(TRUE), mart=ensembl)
 
+
+
+miRNA_counts <- counts %>%
+  mutate(id = rownames(counts)) %>%
+  separate("id", c("ensembl_transcript_id", NA)) %>%
+  inner_join(miRNA) %>%
+  column_to_rownames("mirbase_id") %>%
+  select(-c(ensembl_transcript_id))
+
+
+
+filename <- paste(quant_dir_name, "transcript_counts.csv", sep = "_")
+filepath <- paste("..", filename, sep = "/")
+write.table(x = counts, file = filepath, quote = FALSE, sep = ",", row.names=TRUE, col.names=NA)
+
+filename <- paste(quant_dir_name, "mirna_counts.csv", sep = "_")
+filepath <- paste("..", filename, sep = "/")
+write.table(x = miRNA_counts, file = filepath, quote = FALSE, sep = ",", row.names=TRUE, col.names=NA)
