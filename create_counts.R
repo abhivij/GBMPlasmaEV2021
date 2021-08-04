bee1c188d81bcf0a3e7154f4f1a61d217394089a
abhivij <abhishek.kcvijayan@gmail.com>
2021-08-04 14:27:18 +1000

    create counts from k = 21 index

--- create_counts.R
+++ create_counts.R
@@ -3,7 +3,7 @@ library(tximport)
 library(biomaRt)
 
 base_dir <- "/home/abhivij/UNSW/VafaeeLab/GBMPlasmaEV"
-quant_dir_name <- "quants_subset"
+quant_dir_name <- "quants_k21"
 
 setwd(base_dir)
 
@@ -23,7 +23,7 @@ print(paste("all file exists ?", all(file.exists(files))))
 txi <- tximport(files, type = "salmon", txOut = TRUE)
 counts <- txi$counts
 counts <- as.data.frame(counts) %>%
-  filter(rowSums(counts) > 0)
+  dplyr::filter(rowSums(counts) > 0)
 
 
 
@@ -33,15 +33,26 @@ miRNA <- getBM(attributes=c("mirbase_id","ensembl_transcript_id"),
               filters=c("with_mirbase"),values=list(TRUE), mart=ensembl)
 
 
-
-miRNA_counts <- counts %>%
+counts_modified <- counts %>%
   mutate(id = rownames(counts)) %>%
-  separate("id", c("ensembl_transcript_id", NA)) %>%
-  inner_join(miRNA) %>%
-  column_to_rownames("mirbase_id") %>%
+  separate("id", c("ensembl_transcript_id", NA))
+
+length(counts_modified$ensembl_transcript_id)
+length(unique(counts_modified$ensembl_transcript_id))
+
+miRNA_counts <- counts_modified %>%
+  inner_join(miRNA)
+length(miRNA_counts$ensembl_transcript_id)
+length(miRNA_counts$mirbase_id)
+length(unique(miRNA_counts$ensembl_transcript_id))
+length(unique(miRNA_counts$mirbase_id))
+
+
+counts_filtered_for_miRNA <- counts_modified %>%
+  filter(ensembl_transcript_id %in% unique(miRNA_counts$ensembl_transcript_id))
+rownames(counts_filtered_for_miRNA) <- counts_filtered_for_miRNA$ensembl_transcript_id
+counts_filtered_for_miRNA <- counts_filtered_for_miRNA %>%
   select(-c(ensembl_transcript_id))
-
-
 
 filename <- paste(quant_dir_name, "transcript_counts.csv", sep = "_")
 filepath <- paste("..", filename, sep = "/")
@@ -49,4 +60,13 @@ write.table(x = counts, file = filepath, quote = FALSE, sep = ",", row.names=TRU
 
 filename <- paste(quant_dir_name, "mirna_counts.csv", sep = "_")
 filepath <- paste("..", filename, sep = "/")
-write.table(x = miRNA_counts, file = filepath, quote = FALSE, sep = ",", row.names=TRUE, col.names=NA)
+write.table(x = counts_filtered_for_miRNA, file = filepath, quote = FALSE, sep = ",", row.names=TRUE, col.names=NA)
+
+
+miRNA_subset <- miRNA %>%
+  filter(ensembl_transcript_id %in% unique(miRNA_counts$ensembl_transcript_id))
+length(unique(miRNA_subset$mirbase_id))
+length(unique(miRNA_subset$ensembl_transcript_id))
+
+filename <- paste("mirna_name_mapping.csv", sep = "_")
+write.table(x = miRNA_subset, file = filename, quote = FALSE, sep = ",", row.names = FALSE)
