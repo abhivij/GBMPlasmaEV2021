1dcc7f7f297ec21178ee9fff11d1fbd4eb5e7928
abhivij <abhishek.kcvijayan@gmail.com>
2021-08-26 08:55:14 +1000

    create protein dim red plots

--- scripts/R/plot_data.R
+++ scripts/R/plot_data.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(viridis)
+library(umap)
 setwd("/home/abhivij/UNSW/VafaeeLab/GBMPlasmaEV/scripts/R")
 source("utils.R")
 
@@ -20,10 +21,10 @@ create_box_plot <- function(data, title, filename){
 }
 
 norm_output1 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv")
-create_box_plot(norm_output1, "Q1-7 Equalize median norm data", "q1to7_eqmed.png")
+create_box_plot(norm_output1, "Q1-6 Equalize median norm data", "q1to6_eqmed.png")
 
 norm_output2 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_FALSE.csv")
-create_box_plot(norm_output2, "Q1-7 without norm data", "q1to7_nonorm.png")
+create_box_plot(norm_output2, "Q1-6 without norm data", "q1to6_nonorm.png")
 
 
 file_path <- "Data/Protein/Sumofnormalisedareas/Batch1-18_modified.csv"
@@ -35,10 +36,21 @@ sum_norm_area_data <- data.frame(t(log2(sum_norm_area_data))) %>%
   mutate(GROUP_ORIGINAL = "dummy")
 create_box_plot(sum_norm_area_data, "Log Sum norm area data", "log_sum_norm_area.png")
 
+###################################
+
+norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv")
+groups <- norm_data$GROUP_ORIGINAL
+norm_data <- norm_data %>%
+  select(-c(GROUP_ORIGINAL)) %>%
+  column_to_rownames("SUBJECT_ORIGINAL")
+norm_data <- data.frame(t(norm_data)) %>%
+  drop_na()
+norm_data <- data.frame(t(norm_data))
+text <- NA  
 
 plot_data <- function(norm_data, filename, title, colour_label,
                       perplexity = 30, 
-                      groups, shownames = FALSE, text = NA, dim_red = "tsne"){
+                      groups, shownames = FALSE, text = NA, dim_red = "pca"){
   
   if(shownames && length(text) == 1 && is.na(text)){
     text <- rownames(norm_data)
@@ -54,14 +66,22 @@ plot_data <- function(norm_data, filename, title, colour_label,
     ylab <- "tSNE 2"
   } else if(dim_red == "umap"){
     print(dim(norm_data)[1])
-    n_neighbors <- max(floor(dim(norm_data)[1] / 4), 2)
-    print(n_neighbors)
-    result <- umap(norm_data, n_neighbors = n_neighbors)
+    # n_neighbors <- max(floor(dim(norm_data)[1] / 4), 2)
+    # print(n_neighbors)
+    # result <- umap(norm_data, n_neighbors = n_neighbors)
+    result <- umap(norm_data)
     dim_red_df <- data.frame(x = result$layout[,1], y = result$layout[,2], 
                              Colour = groups, 
                              Sample = text)  
     xlab <- "UMAP 1"
     ylab <- "UMAP 2"
+  } else if(dim_red == "pca"){
+    result <- prcomp(norm_data)
+    dim_red_df <- data.frame(x = result$x[,1], y = result$x[,2], 
+                             Colour = groups, 
+                             Sample = text)    
+    xlab <- "PCA 1"
+    ylab <- "PCA 2"    
   }
   
   
@@ -83,129 +103,118 @@ plot_data <- function(norm_data, filename, title, colour_label,
   }
   
   filename <- paste(dim_red, filename, sep = "_")
-  filename <- paste("plots", filename, sep = "/")
+  filename <- paste("plots/dim_red", filename, sep = "/")
   ggplot2::ggsave(filename, dim_red_plot)  
 }
 
-norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv")
-plot_protein_norm_data <- function(norm_data, dirname = "", filename, 
-                                   title, width = 30, height = 30, perplexity = 30){
+plot_protein_norm_data <- function(norm_data, filename, 
+                                   title, width = 30, height = 30, perplexity = 30,
+                                   dim_red = "pca", colour_label = "Labels"){
   groups <- norm_data$GROUP_ORIGINAL
   norm_data <- norm_data %>%
     select(-c(GROUP_ORIGINAL)) %>%
     column_to_rownames("SUBJECT_ORIGINAL")
-  norm_data <- t(norm_data)
+  norm_data <- data.frame(t(norm_data)) %>%
+    drop_na()
+  norm_data <- data.frame(t(norm_data))
+  
+  plot_data(norm_data, filename, title, colour_label = colour_label,
+                        perplexity = 30, 
+                        groups, shownames = FALSE, text = NA, dim_red)
 }
 
+norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv")
+plot_protein_norm_data(norm_data, "q1to6_eqmed.png", "Q1-6 Equalize median norm data", dim_red = "pca")
+plot_protein_norm_data(norm_data, "q1to6_eqmed.png", "Q1-6 Equalize median norm data", dim_red = "tsne")
+plot_protein_norm_data(norm_data, "q1to6_eqmed.png", "Q1-6 Equalize median norm data", dim_red = "umap")
+
+norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_column_equalizeMedians.csv")
+plot_protein_norm_data(norm_data, "column_q1to6_eqmed.png", "Column Q1-6 Equalize median norm data", dim_red = "pca")
+plot_protein_norm_data(norm_data, "column_q1to6_eqmed.png", "Column Q1-6 Equalize median norm data", dim_red = "tsne")
+plot_protein_norm_data(norm_data, "column_q1to6_eqmed.png", "Column Q1-6 Equalize median norm data", dim_red = "umap")
 
 
-create_plot <- function(norm_data, dirname = "", filename, title, width = 30, height = 30, perplexity = 30){
+norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_column_FALSE.csv")
+plot_protein_norm_data(norm_data, "column_q1to6_nonorm.png", "Column Q1-6 no norm data", dim_red = "pca")
+plot_protein_norm_data(norm_data, "column_q1to6_nonorm.png", "Column Q1-6 no norm data", dim_red = "tsne")
+plot_protein_norm_data(norm_data, "column_q1to6_nonorm.png", "Column Q1-6 no norm data", dim_red = "umap")
+
+norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_disease_equalizeMedians.csv")
+plot_protein_norm_data(norm_data, "disease_q1to6_eqmed.png", "Disease Q1-6 Equalize median norm data", dim_red = "pca")
+plot_protein_norm_data(norm_data, "disease_q1to6_eqmed.png", "Disease Q1-6 Equalize median norm data", dim_red = "tsne")
+plot_protein_norm_data(norm_data, "disease_q1to6_eqmed.png", "Disease Q1-6 Equalize median norm data", dim_red = "umap")
+
+norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_disease_FALSE.csv")
+plot_protein_norm_data(norm_data, "disease_q1to6_nonorm.png", "Disease Q1-6 no norm data", dim_red = "pca")
+plot_protein_norm_data(norm_data, "disease_q1to6_nonorm.png", "Disease Q1-6 no norm data", dim_red = "tsne")
+plot_protein_norm_data(norm_data, "disease_q1to6_nonorm.png", "Disease Q1-6 no norm data", dim_red = "umap")
+
+
+
+norm_data1 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv") %>%
+  select(c(SUBJECT_ORIGINAL, GROUP_ORIGINAL)) %>%
+  rename("Q1to6Condition" = "GROUP_ORIGINAL")
+norm_data5 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_disease_equalizeMedians.csv") %>%
+  select(c(SUBJECT_ORIGINAL, GROUP_ORIGINAL)) %>%
+  rename("DiseaseType" = "GROUP_ORIGINAL")
+norm_data3 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_column_equalizeMedians.csv") %>%
+  select(c(SUBJECT_ORIGINAL, GROUP_ORIGINAL)) %>%
+  rename("Column" = "GROUP_ORIGINAL")
+
+meta_data <- norm_data1 %>%
+  inner_join(norm_data5) %>%
+  full_join(norm_data3)
+
+
   
-  ne <- t(norm_data)
+plot_protein_sum_of_area_data <- function(norm_data, meta_data, condition,
+                                          l = FALSE, filename, 
+                                          title, width = 30, height = 30, perplexity = 30,
+                                          dim_red = "pca", colour_label = "Labels"){
   
-  # PCA on all samples and all proteins
-  all_group_names <- ne["GROUP_ORIGINAL", ]
-  all_sample_names <- ne["SUBJECT_ORIGINAL", ]
-  colnames(ne) <- ne["SUBJECT_ORIGINAL", ]
-  ne <- ne[-which(row.names(ne) %in% c("GROUP_ORIGINAL", "SUBJECT_ORIGINAL")), ]
-  ne <- data.frame(ne)
-  ne <- sapply(ne, function(x) as.numeric(x))  #causes rownames that is protein names to be lost
-  ne <- t(ne)
-  
-  pca_file_name <- append_path(dirname, paste("pca", filename, sep = "_"))
-  p <- prcomp(ne)
-  pca_plotdata <- data.frame(p$x) %>%
-    mutate(type = all_group_names) %>%
-    mutate(name = all_sample_names)
-  
-  tsne_file_name <- append_path(dirname, paste("tsne", filename, sep = "_"))
-  set.seed(1)
-  tsne_result <- Rtsne::Rtsne(ne, perplexity = perplexity)
-  tsne_df <- data.frame(x = tsne_result$Y[,1], y = tsne_result$Y[,2], 
-                        Colour = all_group_names,
-                        name = all_sample_names)
-  
-  if (dim(ne)[1] < 20) {
-    pca_plotdata %>%
-      ggplot(aes(x = PC1, y = PC2, colour = type, label = name)) +
-      geom_point(size = 2) + 
-      labs(title = title) +
-      ggrepel::geom_text_repel(show.legend = FALSE)
-    ggsave(pca_file_name, width = width, height = height, units = "cm")  
-    
-    tsne_plot <- tsne_df %>%
-      ggplot(aes(x = x, y = y, colour = Colour, label = name)) +
-      geom_point(size = 2) +
-      ggplot2::labs(colour = "Groups", title = title) +
-      ggplot2::xlab("tSNE 1") +
-      ggplot2::ylab("tSNE 2") +
-      ggrepel::geom_text_repel(show.legend = FALSE)  
-  } else {
-    pca_plotdata %>%
-      ggplot(aes(x = PC1, y = PC2, colour = type, label = name)) +
-      geom_point(size = 2) + 
-      labs(title = title)
-    ggsave(pca_file_name, width = width, height = height, units = "cm")  
-    
-    tsne_plot <- tsne_df %>%
-      ggplot(aes(x = x, y = y, colour = Colour, label = name)) +
-      geom_point(size = 2) +
-      ggplot2::labs(colour = "Groups", title = title) +
-      ggplot2::xlab("tSNE 1") +
-      ggplot2::ylab("tSNE 2")
+  if(l == TRUE){
+      norm_data <- log2(norm_data)
   }
-  
-  ggplot2::ggsave(tsne_file_name, tsne_plot)
-  
+  meta_data <- meta_data %>%
+    select(c("SUBJECT_ORIGINAL", condition))
+  norm_data <- data.frame(t(norm_data)) %>%
+    rownames_to_column("SUBJECT_ORIGINAL") %>%
+    inner_join(meta_data) %>%
+    rename("GROUP_ORIGINAL" = condition)
+  plot_protein_norm_data(norm_data, filename, 
+                                     title, width = 30, height = 30, perplexity = 30,
+                                     dim_red = dim_red, colour_label = colour_label)
 }
 
-data <- read.csv("Data/Protein/sample_columns.csv")
 
-sample_column <- rbind(data.frame(Sample = data$Column1, Column = 'col1'),
-                       data.frame(Sample = data$Column2, Column = 'col2'),
-                       data.frame(Sample = data$Column3, Column = 'col3')) %>%
-  filter(Sample != "")
-sample_column_qc <- sample_column %>%
-  filter(grepl("HB18|HC7", Sample))
-
-write.csv(sample_column_qc, "Data/Protein/sample_column_qc.csv", row.names = FALSE, quote = FALSE)
+file_path <- "Data/Protein/Sumofnormalisedareas/Batch1-18_modified.csv"
+sum_norm_area_data <- read.table(file_path, header = TRUE, sep = ",", 
+                                 comment.char = "", na.strings = "#N/A",
+                                 row.names = 1)
 
 
-norm_data <- read.csv(file = "Data/normQ1-6.csv")
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "DiseaseType",
+                              filename = "sumnormarea_disease.png", title = "Disease from Sum norm Area data", 
+                              dim_red = "pca")
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "DiseaseType",
+                              filename = "sumnormarea_disease.png", title = "Disease from Sum norm Area data", 
+                              dim_red = "umap")
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "DiseaseType",
+                              filename = "log_sumnormarea_disease.png", title = "Disease from Log Sum norm Area data", 
+                              dim_red = "pca", l = TRUE)
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "DiseaseType",
+                              filename = "log_sumnormarea_disease.png", title = "Disease from Log Sum norm Area data", 
+                              dim_red = "umap", l = TRUE)
 
-sum(is.na(norm_data))
-for(i in c(1:dim(norm_data)[1])){
-  for(j in c(1:dim(norm_data)[2])){
-    if(is.na(norm_data[i,j])){
-      print(paste(i,j))
-      break
-    }
-  }
-}
-
-#filtering out column with missing value
-norm_data <- norm_data %>%
-  select(-c(185))
-
-
-create_plot(norm_data, dirname = "plots/all_q1-6", filename = "all_proteins.jpg", title = "All samples with all proteins")
-
-
-
-norm_data_qc <- norm_data %>%
-  filter(grepl("HB18|HC7", SUBJECT_ORIGINAL)) %>%
-  inner_join(sample_column_qc, by = c("SUBJECT_ORIGINAL" = "Sample")) %>%
-  select(-GROUP_ORIGINAL) %>%
-  rename(GROUP_ORIGINAL = Column)
-
-create_plot(norm_data = norm_data_qc, dirname = "plots/all_q1-6", filename = "qc_proteins.jpg", title = "QC samples with all proteins", 
-            height = 10, width = 10, perplexity = 2)
-
-
-column_data <- norm_data %>%
-  inner_join(sample_column, by = c("SUBJECT_ORIGINAL" = "Sample")) %>%
-  select(-GROUP_ORIGINAL) %>%
-  rename(GROUP_ORIGINAL = Column)
-
-create_plot(norm_data = column_data, dirname = "plots/all_q1-6", filename = "all_proteins_by_col.jpg", title = "All samples with all proteins based on columns", 
-            height = 10, width = 10)
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "Column",
+                              filename = "sumnormarea_column.png", title = "Column from Sum norm Area data", 
+                              dim_red = "pca")
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "Column",
+                              filename = "sumnormarea_column.png", title = "Column from Sum norm Area data", 
+                              dim_red = "umap")
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "Column",
+                              filename = "log_sumnormarea_column.png", title = "Column from Log Sum norm Area data", 
+                              dim_red = "pca", l = TRUE)
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "Column",
+                              filename = "log_sumnormarea_column.png", title = "Column from Log Sum norm Area data", 
+                              dim_red = "umap", l = TRUE)
