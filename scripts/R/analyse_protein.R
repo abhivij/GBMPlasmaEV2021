693a7fb08856d3601723a1cd72803af56fd1e034
abhivij <abhishek.kcvijayan@gmail.com>
2021-08-26 09:16:02 +1000

    code restructure

---
+++ scripts/R/analyse_protein.R
@@ -0,0 +1,105 @@
+library(tidyverse)
+library(readxl)
+library(edgeR)
+library(caret)
+
+base_dir <- "/home/abhivij/UNSW/VafaeeLab/GBMPlasmaEV"
+setwd(base_dir)
+
+source("scripts/R/utils.R")
+source("scripts/R/plot_data.R")
+
+norm_output1 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv")
+create_box_plot(norm_output1, "Q1-6 Equalize median norm data", "q1to6_eqmed.png")
+
+norm_output2 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_FALSE.csv")
+create_box_plot(norm_output2, "Q1-6 without norm data", "q1to6_nonorm.png")
+
+
+file_path <- "Data/Protein/Sumofnormalisedareas/Batch1-18_modified.csv"
+sum_norm_area_data <- read.table(file_path, header = TRUE, sep = ",", 
+                                 comment.char = "", na.strings = "#N/A",
+                                 row.names = 1)
+sum_norm_area_data <- data.frame(t(log2(sum_norm_area_data))) %>%
+  rownames_to_column("SUBJECT_ORIGINAL") %>%
+  mutate(GROUP_ORIGINAL = "dummy")
+create_box_plot(sum_norm_area_data, "Log Sum norm area data", "log_sum_norm_area.png")
+
+norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv")
+plot_protein_norm_data(norm_data, "q1to6_eqmed.png", "Q1-6 Equalize median norm data", dim_red = "pca")
+plot_protein_norm_data(norm_data, "q1to6_eqmed.png", "Q1-6 Equalize median norm data", dim_red = "tsne")
+plot_protein_norm_data(norm_data, "q1to6_eqmed.png", "Q1-6 Equalize median norm data", dim_red = "umap")
+
+norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_column_equalizeMedians.csv")
+plot_protein_norm_data(norm_data, "column_q1to6_eqmed.png", "Column Q1-6 Equalize median norm data", dim_red = "pca")
+plot_protein_norm_data(norm_data, "column_q1to6_eqmed.png", "Column Q1-6 Equalize median norm data", dim_red = "tsne")
+plot_protein_norm_data(norm_data, "column_q1to6_eqmed.png", "Column Q1-6 Equalize median norm data", dim_red = "umap")
+
+
+norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_column_FALSE.csv")
+plot_protein_norm_data(norm_data, "column_q1to6_nonorm.png", "Column Q1-6 no norm data", dim_red = "pca")
+plot_protein_norm_data(norm_data, "column_q1to6_nonorm.png", "Column Q1-6 no norm data", dim_red = "tsne")
+plot_protein_norm_data(norm_data, "column_q1to6_nonorm.png", "Column Q1-6 no norm data", dim_red = "umap")
+
+norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_disease_equalizeMedians.csv")
+plot_protein_norm_data(norm_data, "disease_q1to6_eqmed.png", "Disease Q1-6 Equalize median norm data", dim_red = "pca")
+plot_protein_norm_data(norm_data, "disease_q1to6_eqmed.png", "Disease Q1-6 Equalize median norm data", dim_red = "tsne")
+plot_protein_norm_data(norm_data, "disease_q1to6_eqmed.png", "Disease Q1-6 Equalize median norm data", dim_red = "umap")
+
+norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_disease_FALSE.csv")
+plot_protein_norm_data(norm_data, "disease_q1to6_nonorm.png", "Disease Q1-6 no norm data", dim_red = "pca")
+plot_protein_norm_data(norm_data, "disease_q1to6_nonorm.png", "Disease Q1-6 no norm data", dim_red = "tsne")
+plot_protein_norm_data(norm_data, "disease_q1to6_nonorm.png", "Disease Q1-6 no norm data", dim_red = "umap")
+
+
+
+norm_data1 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv") %>%
+  select(c(SUBJECT_ORIGINAL, GROUP_ORIGINAL)) %>%
+  rename("Q1to6Condition" = "GROUP_ORIGINAL")
+norm_data5 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_disease_equalizeMedians.csv") %>%
+  select(c(SUBJECT_ORIGINAL, GROUP_ORIGINAL)) %>%
+  rename("DiseaseType" = "GROUP_ORIGINAL")
+norm_data3 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_column_equalizeMedians.csv") %>%
+  select(c(SUBJECT_ORIGINAL, GROUP_ORIGINAL)) %>%
+  rename("Column" = "GROUP_ORIGINAL")
+
+meta_data <- norm_data1 %>%
+  inner_join(norm_data5) %>%
+  full_join(norm_data3)
+
+
+
+
+
+
+file_path <- "Data/Protein/Sumofnormalisedareas/Batch1-18_modified.csv"
+sum_norm_area_data <- read.table(file_path, header = TRUE, sep = ",", 
+                                 comment.char = "", na.strings = "#N/A",
+                                 row.names = 1)
+
+
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "DiseaseType",
+                              filename = "sumnormarea_disease.png", title = "Disease from Sum norm Area data", 
+                              dim_red = "pca")
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "DiseaseType",
+                              filename = "sumnormarea_disease.png", title = "Disease from Sum norm Area data", 
+                              dim_red = "umap")
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "DiseaseType",
+                              filename = "log_sumnormarea_disease.png", title = "Disease from Log Sum norm Area data", 
+                              dim_red = "pca", l = TRUE)
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "DiseaseType",
+                              filename = "log_sumnormarea_disease.png", title = "Disease from Log Sum norm Area data", 
+                              dim_red = "umap", l = TRUE)
+
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "Column",
+                              filename = "sumnormarea_column.png", title = "Column from Sum norm Area data", 
+                              dim_red = "pca")
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "Column",
+                              filename = "sumnormarea_column.png", title = "Column from Sum norm Area data", 
+                              dim_red = "umap")
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "Column",
+                              filename = "log_sumnormarea_column.png", title = "Column from Log Sum norm Area data", 
+                              dim_red = "pca", l = TRUE)
+plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "Column",
+                              filename = "log_sumnormarea_column.png", title = "Column from Log Sum norm Area data", 
+                              dim_red = "umap", l = TRUE)
--- scripts/R/plot_data.R
+++ scripts/R/plot_data.R
@@ -20,33 +20,18 @@ create_box_plot <- function(data, title, filename){
   ggsave(filename, box_plot)  
 }
 
-norm_output1 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv")
-create_box_plot(norm_output1, "Q1-6 Equalize median norm data", "q1to6_eqmed.png")
-
-norm_output2 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_FALSE.csv")
-create_box_plot(norm_output2, "Q1-6 without norm data", "q1to6_nonorm.png")
-
-
-file_path <- "Data/Protein/Sumofnormalisedareas/Batch1-18_modified.csv"
-sum_norm_area_data <- read.table(file_path, header = TRUE, sep = ",", 
-                                 comment.char = "", na.strings = "#N/A",
-                                 row.names = 1)
-sum_norm_area_data <- data.frame(t(log2(sum_norm_area_data))) %>%
-  rownames_to_column("SUBJECT_ORIGINAL") %>%
-  mutate(GROUP_ORIGINAL = "dummy")
-create_box_plot(sum_norm_area_data, "Log Sum norm area data", "log_sum_norm_area.png")
 
 ###################################
 
-norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv")
-groups <- norm_data$GROUP_ORIGINAL
-norm_data <- norm_data %>%
-  select(-c(GROUP_ORIGINAL)) %>%
-  column_to_rownames("SUBJECT_ORIGINAL")
-norm_data <- data.frame(t(norm_data)) %>%
-  drop_na()
-norm_data <- data.frame(t(norm_data))
-text <- NA  
+# norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv")
+# groups <- norm_data$GROUP_ORIGINAL
+# norm_data <- norm_data %>%
+#   select(-c(GROUP_ORIGINAL)) %>%
+#   column_to_rownames("SUBJECT_ORIGINAL")
+# norm_data <- data.frame(t(norm_data)) %>%
+#   drop_na()
+# norm_data <- data.frame(t(norm_data))
+# text <- NA  
 
 plot_data <- function(norm_data, filename, title, colour_label,
                       perplexity = 30, 
@@ -123,57 +108,13 @@ plot_protein_norm_data <- function(norm_data, filename,
                         groups, shownames = FALSE, text = NA, dim_red)
 }
 
-norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv")
-plot_protein_norm_data(norm_data, "q1to6_eqmed.png", "Q1-6 Equalize median norm data", dim_red = "pca")
-plot_protein_norm_data(norm_data, "q1to6_eqmed.png", "Q1-6 Equalize median norm data", dim_red = "tsne")
-plot_protein_norm_data(norm_data, "q1to6_eqmed.png", "Q1-6 Equalize median norm data", dim_red = "umap")
-
-norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_column_equalizeMedians.csv")
-plot_protein_norm_data(norm_data, "column_q1to6_eqmed.png", "Column Q1-6 Equalize median norm data", dim_red = "pca")
-plot_protein_norm_data(norm_data, "column_q1to6_eqmed.png", "Column Q1-6 Equalize median norm data", dim_red = "tsne")
-plot_protein_norm_data(norm_data, "column_q1to6_eqmed.png", "Column Q1-6 Equalize median norm data", dim_red = "umap")
-
-
-norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_column_FALSE.csv")
-plot_protein_norm_data(norm_data, "column_q1to6_nonorm.png", "Column Q1-6 no norm data", dim_red = "pca")
-plot_protein_norm_data(norm_data, "column_q1to6_nonorm.png", "Column Q1-6 no norm data", dim_red = "tsne")
-plot_protein_norm_data(norm_data, "column_q1to6_nonorm.png", "Column Q1-6 no norm data", dim_red = "umap")
-
-norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_disease_equalizeMedians.csv")
-plot_protein_norm_data(norm_data, "disease_q1to6_eqmed.png", "Disease Q1-6 Equalize median norm data", dim_red = "pca")
-plot_protein_norm_data(norm_data, "disease_q1to6_eqmed.png", "Disease Q1-6 Equalize median norm data", dim_red = "tsne")
-plot_protein_norm_data(norm_data, "disease_q1to6_eqmed.png", "Disease Q1-6 Equalize median norm data", dim_red = "umap")
-
-norm_data <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_disease_FALSE.csv")
-plot_protein_norm_data(norm_data, "disease_q1to6_nonorm.png", "Disease Q1-6 no norm data", dim_red = "pca")
-plot_protein_norm_data(norm_data, "disease_q1to6_nonorm.png", "Disease Q1-6 no norm data", dim_red = "tsne")
-plot_protein_norm_data(norm_data, "disease_q1to6_nonorm.png", "Disease Q1-6 no norm data", dim_red = "umap")
-
-
-
-norm_data1 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_NA_equalizeMedians.csv") %>%
-  select(c(SUBJECT_ORIGINAL, GROUP_ORIGINAL)) %>%
-  rename("Q1to6Condition" = "GROUP_ORIGINAL")
-norm_data5 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_disease_equalizeMedians.csv") %>%
-  select(c(SUBJECT_ORIGINAL, GROUP_ORIGINAL)) %>%
-  rename("DiseaseType" = "GROUP_ORIGINAL")
-norm_data3 <- read.csv(file = "Data/Protein/output/norm_annotatedQ1-6_column_equalizeMedians.csv") %>%
-  select(c(SUBJECT_ORIGINAL, GROUP_ORIGINAL)) %>%
-  rename("Column" = "GROUP_ORIGINAL")
-
-meta_data <- norm_data1 %>%
-  inner_join(norm_data5) %>%
-  full_join(norm_data3)
-
-
-  
 plot_protein_sum_of_area_data <- function(norm_data, meta_data, condition,
                                           l = FALSE, filename, 
                                           title, width = 30, height = 30, perplexity = 30,
                                           dim_red = "pca", colour_label = "Labels"){
   
   if(l == TRUE){
-      norm_data <- log2(norm_data)
+    norm_data <- log2(norm_data)
   }
   meta_data <- meta_data %>%
     select(c("SUBJECT_ORIGINAL", condition))
@@ -182,39 +123,6 @@ plot_protein_sum_of_area_data <- function(norm_data, meta_data, condition,
     inner_join(meta_data) %>%
     rename("GROUP_ORIGINAL" = condition)
   plot_protein_norm_data(norm_data, filename, 
-                                     title, width = 30, height = 30, perplexity = 30,
-                                     dim_red = dim_red, colour_label = colour_label)
+                         title, width = 30, height = 30, perplexity = 30,
+                         dim_red = dim_red, colour_label = colour_label)
 }
-
-
-file_path <- "Data/Protein/Sumofnormalisedareas/Batch1-18_modified.csv"
-sum_norm_area_data <- read.table(file_path, header = TRUE, sep = ",", 
-                                 comment.char = "", na.strings = "#N/A",
-                                 row.names = 1)
-
-
-plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "DiseaseType",
-                              filename = "sumnormarea_disease.png", title = "Disease from Sum norm Area data", 
-                              dim_red = "pca")
-plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "DiseaseType",
-                              filename = "sumnormarea_disease.png", title = "Disease from Sum norm Area data", 
-                              dim_red = "umap")
-plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "DiseaseType",
-                              filename = "log_sumnormarea_disease.png", title = "Disease from Log Sum norm Area data", 
-                              dim_red = "pca", l = TRUE)
-plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "DiseaseType",
-                              filename = "log_sumnormarea_disease.png", title = "Disease from Log Sum norm Area data", 
-                              dim_red = "umap", l = TRUE)
-
-plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "Column",
-                              filename = "sumnormarea_column.png", title = "Column from Sum norm Area data", 
-                              dim_red = "pca")
-plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "Column",
-                              filename = "sumnormarea_column.png", title = "Column from Sum norm Area data", 
-                              dim_red = "umap")
-plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "Column",
-                              filename = "log_sumnormarea_column.png", title = "Column from Log Sum norm Area data", 
-                              dim_red = "pca", l = TRUE)
-plot_protein_sum_of_area_data(sum_norm_area_data, meta_data, "Column",
-                              filename = "log_sumnormarea_column.png", title = "Column from Log Sum norm Area data", 
-                              dim_red = "umap", l = TRUE)
