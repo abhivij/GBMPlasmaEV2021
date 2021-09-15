896979478c754d3f58c2abcbee9ad2fa840d6d07
abhivij <abhishek.kcvijayan@gmail.com>
2021-09-15 20:54:14 +1000

    create formatted data and metadata

---
+++ scripts/R/transcriptomic/create_formatted_data.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+
+
+base_dir <- "/home/abhivij/UNSW/VafaeeLab/GBMPlasmaEV"
+setwd(base_dir)
+
+
+data <- read_excel("Data/RNA/158629.all_samples.summary.xlsx", sheet = "miRNA_piRNA")
+mirna_data <- data[1:2505,]
+pirna_data <- data[2507:2642,] %>%
+  separate(miRNA, c("miRNA", NA, NA, NA), sep = "/")
+
+dim(data)[1]
+dim(mirna_data)[1] + dim(pirna_data)[1]
+
+data <- rbind(mirna_data, pirna_data) %>%
+  column_to_rownames("miRNA")
+
+umi_counts <- data %>%
+  select(ends_with("UMIs"))
+colnames(umi_counts) <- gsub("-UMIs", "", colnames(umi_counts))
+colnames(umi_counts) <- sapply(colnames(umi_counts), FUN = 
+                                 function(x){
+                                   strsplit(x, split = "_", fixed = TRUE)[[1]][1]
+                                 }
+)
+
+write.csv(umi_counts, "Data/RNA/umi_counts.csv")
--- scripts/R/transcriptomic/create_metadata.R
+++ scripts/R/transcriptomic/create_metadata.R
@@ -3,33 +3,7 @@ library(tidyverse)
 base_dir <- "/home/abhivij/UNSW/VafaeeLab/GBMPlasmaEV"
 setwd(base_dir)
 
-source("scripts/R/utils.R")
-
-data <- read_excel("Data/RNA/158629.all_samples.summary.xlsx", sheet = "miRNA_piRNA")
-mirna_data <- data[1:2505,]
-pirna_data <- data[2507:2642,] %>%
-  separate(miRNA, c("miRNA", NA, NA, NA), sep = "/")
-
-dim(data)[1]
-dim(mirna_data)[1] + dim(pirna_data)[1]
-
-data <- rbind(mirna_data, pirna_data) %>%
-  column_to_rownames("miRNA")
-
-umi_counts <- data %>%
-  select(ends_with("UMIs"))
-colnames(umi_counts) <- gsub("-UMIs", "", colnames(umi_counts))
-colnames(umi_counts) <- sapply(colnames(umi_counts), FUN = 
-                                 function(x){
-                                   strsplit(x, split = "_", fixed = TRUE)[[1]][1]
-                                 }
-)
-
-
-
-
-
-#to get meta data start
+umi_counts <- read.csv("Data/RNA/umi_counts.csv", row.names = 1)
 
 #get group mappings from Q1-6 proteomics output
 protein_data <- read.csv(file = "Data/Protein/norm_output/norm_annotatedQ1-6_NA_equalizeMedians.csv")
