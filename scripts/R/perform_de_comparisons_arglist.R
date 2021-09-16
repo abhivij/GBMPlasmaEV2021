01b28e48a246622c34f47daacf66a9c8a4147924
abhivij <abhishek.kcvijayan@gmail.com>
2021-09-16 10:28:15 +1000

    DE from quantile norm results

--- scripts/R/perform_de_comparisons_arglist.R
+++ scripts/R/perform_de_comparisons_arglist.R
@@ -34,5 +34,46 @@ perform_de_comparisons_arglist <- list(
   #7
   list(file_name = "data_process_output_annotatedQ7_NA_equalizeMedians.rds", 
        comparison_list = list(c("PREOPE", "REC-TP")), 
-       comparison_num = "7")  
+       comparison_num = "7"),
+  
+  #8
+  list(file_name = "data_process_output_annotatedQ1-6_NA_quantile.rds", 
+       comparison_list = list(c("PREOPE", "MET"),
+                              c("PREOPE", "HC"),
+                              c("MET", "HC")), 
+       comparison_num = "2"),  
+  
+  #9
+  list(file_name = "data_process_output_annotatedQ1-6_NA_quantile.rds", 
+       comparison_list = list(c("PREOPE", "POSTOPE-T"),
+                              c("PREOPE", "POSTOPE-P"),
+                              c("POSTOPE-T", "POSTOPE-P")), 
+       comparison_num = "3"), 
+  
+  #10
+  list(file_name = "data_process_output_annotatedQ1-6_NA_quantile.rds", 
+       comparison_list = list(c("POSTOPE-T", "REC-T")), 
+       comparison_num = "4"),   
+  
+  #11
+  list(file_name = "data_process_output_annotatedQ1-6_NA_quantile.rds", 
+       comparison_list = list(c("POSTOPE-P", "REC-P")), 
+       comparison_num = "5"), 
+  
+  #12
+  list(file_name = "data_process_output_annotatedQ1-6_NA_quantile.rds", 
+       comparison_list = list(c("POSTOPE-T", "PREREC")), 
+       comparison_num = "6"),     
+  
+  #13
+  list(file_name = "data_process_output_annotatedQ7_NA_quantile.rds", 
+       comparison_list = list(c("PREOPE", "REC-TP")), 
+       comparison_num = "7"),
+
+  #14
+  list(file_name = "data_process_output_annotatedQ1-6_NA_remove_50_missing_quantile.rds", 
+       comparison_list = list(c("PREOPE", "MET"),
+                              c("PREOPE", "HC"),
+                              c("MET", "HC")), 
+       comparison_num = "2")    
 )
--- scripts/katana/perform_de_comparisons_main.sh
+++ scripts/katana/perform_de_comparisons_main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qsub -l select=1:ncpus=8:mem=64gb,walltime=12:00:00 -J 2-7 perform_de_comparisons_main.pbs
+qsub -l select=1:ncpus=8:mem=64gb,walltime=12:00:00 -J 2-14 perform_de_comparisons_main.pbs
