0f427b81778bc0f3dd26d1be96703ba9cbe6d487
abhivij <abhishek.kcvijayan@gmail.com>
2021-09-29 23:10:02 +1000

    proteomics FEM run

--- scripts/R/dataset_pipeline_arguments.R
+++ scripts/R/dataset_pipeline_arguments.R
@@ -89,6 +89,201 @@ dataset_pipeline_arguments <- list(
        classes = c("HC", "MET"),
        cores = 16,
        results_dir_path = "fem_pipeline_results",
-       fems_to_run = c("RF_RFE", "ga_rf"))           
+       fems_to_run = c("RF_RFE", "ga_rf")),
+  
+  #7
+  #proteomic PREOPEVsMET
+  list(phenotype_file_name = "Data/proteomic_phenotype.txt",
+       read_count_dir_path = "Data/Protein/formatted_data",
+       read_count_file_name = "Q1-6_nonorm_formatted.csv",
+       sep = ",",
+       dataset_id = "GBMPlasmaEV_proteomic_quantile",
+       classification_criteria = "PREOPEVsMET",
+       classes = c("MET", "PREOPE"),
+       cores = 16,
+       results_dir_path = "fem_pipeline_results",
+       fems_to_run = c("all", "t-test", "t-test_BH",
+                       "wilcoxontest", "wilcoxontest_BH",
+                       "ranger_impu_cor", 
+                       "mrmr30", "mrmr50", "mrmr75", "mrmr100"),
+       norm = "quantile"),
+  
+  #8
+  #proteomic PREOPEVsHC
+  list(phenotype_file_name = "Data/proteomic_phenotype.txt",
+       read_count_dir_path = "Data/Protein/formatted_data",
+       read_count_file_name = "Q1-6_nonorm_formatted.csv",
+       sep = ",",
+       dataset_id = "GBMPlasmaEV_proteomic_quantile",
+       classification_criteria = "PREOPEVsHC",
+       classes = c("HC", "PREOPE"),
+       cores = 16,
+       results_dir_path = "fem_pipeline_results",
+       fems_to_run = c("all", "t-test", "t-test_BH",
+                       "wilcoxontest", "wilcoxontest_BH",
+                       "ranger_impu_cor", 
+                       "mrmr30", "mrmr50", "mrmr75", "mrmr100"),
+       norm = "quantile"),
+  
+  #9
+  #proteomic METVsHC
+  list(phenotype_file_name = "Data/proteomic_phenotype.txt",
+       read_count_dir_path = "Data/Protein/formatted_data",
+       read_count_file_name = "Q1-6_nonorm_formatted.csv",
+       sep = ",",
+       dataset_id = "GBMPlasmaEV_proteomic_quantile",
+       classification_criteria = "METVsHC",
+       classes = c("HC", "MET"),
+       cores = 16,
+       results_dir_path = "fem_pipeline_results",
+       fems_to_run = c("all", "t-test", "t-test_BH",
+                       "wilcoxontest", "wilcoxontest_BH",
+                       "ranger_impu_cor", 
+                       "mrmr30", "mrmr50", "mrmr75", "mrmr100"),
+       norm = "quantile"),  
+  
+  
+  #10
+  #proteomic PREOPEVsMET
+  # to run "RF_RFE", "ga_rf"
+  list(phenotype_file_name = "Data/proteomic_phenotype.txt",
+       read_count_dir_path = "Data/Protein/formatted_data",
+       read_count_file_name = "Q1-6_nonorm_formatted.csv",
+       sep = ",",
+       dataset_id = "GBMPlasmaEV_proteomic_quantile",
+       classification_criteria = "PREOPEVsMET",
+       classes = c("MET", "PREOPE"),
+       cores = 16,
+       results_dir_path = "fem_pipeline_results",
+       fems_to_run = c("RF_RFE", "ga_rf"),
+       norm = "quantile"),
+  
+  #11
+  #proteomic PREOPEVsHC
+  # to run "RF_RFE", "ga_rf"
+  list(phenotype_file_name = "Data/proteomic_phenotype.txt",
+       read_count_dir_path = "Data/Protein/formatted_data",
+       read_count_file_name = "Q1-6_nonorm_formatted.csv",
+       sep = ",",
+       dataset_id = "GBMPlasmaEV_proteomic_quantile",
+       classification_criteria = "PREOPEVsHC",
+       classes = c("HC", "PREOPE"),
+       cores = 16,
+       results_dir_path = "fem_pipeline_results",
+       fems_to_run = c("RF_RFE", "ga_rf"),
+       norm = "quantile"),
+  
+  #12
+  #proteomic METVsHC
+  # to run "RF_RFE", "ga_rf"
+  list(phenotype_file_name = "Data/proteomic_phenotype.txt",
+       read_count_dir_path = "Data/Protein/formatted_data",
+       read_count_file_name = "Q1-6_nonorm_formatted.csv",
+       sep = ",",
+       dataset_id = "GBMPlasmaEV_proteomic_quantile",
+       classification_criteria = "METVsHC",
+       classes = c("HC", "MET"),
+       cores = 16,
+       results_dir_path = "fem_pipeline_results",
+       fems_to_run = c("RF_RFE", "ga_rf"),
+       norm = "quantile"),
+
+  #13
+  #proteomic PREOPEVsMET
+  list(phenotype_file_name = "Data/proteomic_phenotype.txt",
+       read_count_dir_path = "Data/Protein/formatted_data",
+       read_count_file_name = "Q1-6_nonorm_formatted.csv",
+       sep = ",",
+       dataset_id = "GBMPlasmaEV_proteomic_norm_quantile",
+       classification_criteria = "PREOPEVsMET",
+       classes = c("MET", "PREOPE"),
+       cores = 16,
+       results_dir_path = "fem_pipeline_results",
+       fems_to_run = c("all", "t-test", "t-test_BH",
+                       "wilcoxontest", "wilcoxontest_BH",
+                       "ranger_impu_cor", 
+                       "mrmr30", "mrmr50", "mrmr75", "mrmr100"),
+       norm = "norm_quantile"),
+  
+  #14
+  #proteomic PREOPEVsHC
+  list(phenotype_file_name = "Data/proteomic_phenotype.txt",
+       read_count_dir_path = "Data/Protein/formatted_data",
+       read_count_file_name = "Q1-6_nonorm_formatted.csv",
+       sep = ",",
+       dataset_id = "GBMPlasmaEV_proteomic_norm_quantile",
+       classification_criteria = "PREOPEVsHC",
+       classes = c("HC", "PREOPE"),
+       cores = 16,
+       results_dir_path = "fem_pipeline_results",
+       fems_to_run = c("all", "t-test", "t-test_BH",
+                       "wilcoxontest", "wilcoxontest_BH",
+                       "ranger_impu_cor", 
+                       "mrmr30", "mrmr50", "mrmr75", "mrmr100"),
+       norm = "norm_quantile"),
+  
+  #15
+  #proteomic METVsHC
+  list(phenotype_file_name = "Data/proteomic_phenotype.txt",
+       read_count_dir_path = "Data/Protein/formatted_data",
+       read_count_file_name = "Q1-6_nonorm_formatted.csv",
+       sep = ",",
+       dataset_id = "GBMPlasmaEV_proteomic_norm_quantile",
+       classification_criteria = "METVsHC",
+       classes = c("HC", "MET"),
+       cores = 16,
+       results_dir_path = "fem_pipeline_results",
+       fems_to_run = c("all", "t-test", "t-test_BH",
+                       "wilcoxontest", "wilcoxontest_BH",
+                       "ranger_impu_cor", 
+                       "mrmr30", "mrmr50", "mrmr75", "mrmr100"),
+       norm = "norm_quantile"),  
+  
+  
+  #16
+  #proteomic PREOPEVsMET
+  # to run "RF_RFE", "ga_rf"
+  list(phenotype_file_name = "Data/proteomic_phenotype.txt",
+       read_count_dir_path = "Data/Protein/formatted_data",
+       read_count_file_name = "Q1-6_nonorm_formatted.csv",
+       sep = ",",
+       dataset_id = "GBMPlasmaEV_proteomic_norm_quantile",
+       classification_criteria = "PREOPEVsMET",
+       classes = c("MET", "PREOPE"),
+       cores = 16,
+       results_dir_path = "fem_pipeline_results",
+       fems_to_run = c("RF_RFE", "ga_rf"),
+       norm = "norm_quantile"),
+  
+  #17
+  #proteomic PREOPEVsHC
+  # to run "RF_RFE", "ga_rf"
+  list(phenotype_file_name = "Data/proteomic_phenotype.txt",
+       read_count_dir_path = "Data/Protein/formatted_data",
+       read_count_file_name = "Q1-6_nonorm_formatted.csv",
+       sep = ",",
+       dataset_id = "GBMPlasmaEV_proteomic_norm_quantile",
+       classification_criteria = "PREOPEVsHC",
+       classes = c("HC", "PREOPE"),
+       cores = 16,
+       results_dir_path = "fem_pipeline_results",
+       fems_to_run = c("RF_RFE", "ga_rf"),
+       norm = "norm_quantile"),
+  
+  #18
+  #proteomic METVsHC
+  # to run "RF_RFE", "ga_rf"
+  list(phenotype_file_name = "Data/proteomic_phenotype.txt",
+       read_count_dir_path = "Data/Protein/formatted_data",
+       read_count_file_name = "Q1-6_nonorm_formatted.csv",
+       sep = ",",
+       dataset_id = "GBMPlasmaEV_proteomic_norm_quantile",
+       classification_criteria = "METVsHC",
+       classes = c("HC", "MET"),
+       cores = 16,
+       results_dir_path = "fem_pipeline_results",
+       fems_to_run = c("RF_RFE", "ga_rf"),
+       norm = "norm_quantile")      
+    
 
 )  
--- scripts/katana/pipeline_executor.sh
+++ scripts/katana/pipeline_executor.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 qsub -l select=1:ncpus=16:mem=124gb,walltime=48:00:00 -J 1-6 pipeline_executor.pbs
+
+qsub -l select=1:ncpus=16:mem=124gb,walltime=48:00:00 -J 7-18 pipeline_executor.pbs
