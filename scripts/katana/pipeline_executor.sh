07ad12e1a3bb1c69d8c496b391c0fcc477999dd7
abhivij <abhishek.kcvijayan@gmail.com>
2021-10-14 13:17:45 +1100

    more fsms and imputed datasets on pipeline

--- scripts/katana/pipeline_executor.sh
+++ scripts/katana/pipeline_executor.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-qsub -l select=1:ncpus=16:mem=124gb,walltime=48:00:00 -J 1-6 pipeline_executor.pbs
+qsub -l select=1:ncpus=16:mem=124gb,walltime=24:00:00 -J 1-6 pipeline_executor.pbs
 
-qsub -l select=1:ncpus=16:mem=124gb,walltime=48:00:00 -J 7-18 pipeline_executor.pbs
+qsub -l select=1:ncpus=16:mem=124gb,walltime=24:00:00 -J 7-12 pipeline_executor.pbs
+
+qsub -l select=1:ncpus=16:mem=124gb,walltime=24:00:00 -J 13-18 pipeline_executor.pbs
+
+qsub -l select=1:ncpus=16:mem=124gb,walltime=24:00:00 -J 19-24 pipeline_executor.pbs
+
+qsub -l select=1:ncpus=16:mem=124gb,walltime=24:00:00 -J 25-30 pipeline_executor.pbs
