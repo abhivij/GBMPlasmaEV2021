87482bcffae477ad5f710d61291ff53e1ce8ca59
abhivij <abhishek.kcvijayan@gmail.com>
2021-11-17 21:53:50 +1100

    proteomic norm comparison

--- scripts/katana/pipeline_executor.sh
+++ scripts/katana/pipeline_executor.sh
@@ -11,3 +11,9 @@ qsub -l select=1:ncpus=16:mem=124gb,walltime=24:00:00 -J 19-24 pipeline_executor
 qsub -l select=1:ncpus=16:mem=124gb,walltime=24:00:00 -J 25-30 pipeline_executor.pbs
 
 qsub -l select=1:ncpus=4:mem=124gb,walltime=10:00:00 -J 31-40 pipeline_executor.pbs
+
+qsub -l select=1:ncpus=16:mem=124gb,walltime=24:00:00 -J 41-46 pipeline_executor.pbs
+
+qsub -l select=1:ncpus=16:mem=124gb,walltime=24:00:00 -J 47-52 pipeline_executor.pbs
+
+qsub -l select=1:ncpus=16:mem=124gb,walltime=24:00:00 -J 53-58 pipeline_executor.pbs
