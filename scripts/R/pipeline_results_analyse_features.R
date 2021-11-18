053a462523a8ffa1c8369cfe9179fca55d6cbd2e
abhivij <abhishek.kcvijayan@gmail.com>
2021-11-18 12:21:28 +1100

    improved features analyse

--- scripts/R/pipeline_results_analyse_features.R
+++ scripts/R/pipeline_results_analyse_features.R
@@ -40,11 +40,13 @@ fsm_vec <- c("all",
 
 # best_fsm <- "mrmr100"
 
+dataset_id <- "GBMPlasmaEV_transcriptomic_PREOPEVsMET"
+
 best_fsm_vec <- c("ranger_impu_cor",
                   "t-test", "wilcoxontest",
                   "mrmr30", "mrmr100")
 
-min_iter_feature_presence = 29
+min_iter_feature_presence = 28
 create_common_feature_plots_and_datasubsets <- function(dataset_id,
                                                         best_fsm_vec, 
                                                         min_iter_feature_presence){
@@ -54,6 +56,7 @@ create_common_feature_plots_and_datasubsets <- function(dataset_id,
   print(features_file)
   
   selected_features <- list()
+  max_size <- 0  #for upset plot max bar length
   for(best_fsm in best_fsm_vec){
     features_info <- read.table(features_file, sep = ',', header = TRUE)
     
@@ -62,8 +65,12 @@ create_common_feature_plots_and_datasubsets <- function(dataset_id,
       select(-c(FSM, Iter))
     print(best_fsm)
     print(dim(features_info[,colSums(features_info) >= min_iter_feature_presence]))
-    
     selected_features[[best_fsm]] <- colnames(features_info[,colSums(features_info) >= min_iter_feature_presence])   
+    
+    size <- length(selected_features[[best_fsm]])
+    if(size > max_size){
+      max_size <- size
+    }
   }
   ###########################################
   #upsetplot
@@ -77,35 +84,92 @@ create_common_feature_plots_and_datasubsets <- function(dataset_id,
   
   print("creating upset plot...")
   print(paste(dir_path, file_name, sep = "/"))
-  png(filename = paste(dir_path, file_name, sep = "/"), 
-      units = "cm", width = 20, height = 15, res = 1200)
-  upset(fromList(selected_features))
+  # dev.copy(png, paste(dir_path, file_name, sep = "/"), 
+  #          units = "cm", width = 30, height = 15, res = 1200)
+  png(filename = paste(dir_path, file_name, sep = "/"),
+      units = "cm", width = 30, height = 15, res = 1200)
+  print({
+    upset(fromList(selected_features), set_size.show = TRUE,  
+          set_size.scale_max = max_size + 20)  
+  })
+  grid.text(paste(dataset_id, min_iter_feature_presence),
+            x = 0.6, y = 0.95)
   dev.off()
   
-  #create new data subsets
   
-  data <- read.table("Data/RNA/umi_counts.csv", header=TRUE, sep=",", row.names=1, skip=0,
-                     nrows=-1, comment.char="", fill=TRUE, na.strings = "NA")
+  ######create new data subsets
+  
+  if(grepl(pattern = "transcriptomic", x = dataset_id, fixed = TRUE)){
+    data <- read.table("Data/RNA/umi_counts.csv", header=TRUE, sep=",", row.names=1, skip=0,
+                       nrows=-1, comment.char="", fill=TRUE, na.strings = "NA")   
+    output_dir <- "Data/RNA/"
+  } else if(grepl(pattern = "proteomic", x = dataset_id, fixed = TRUE)){
+    if(grepl(pattern = "REC-TP", x = dataset_id, fixed = TRUE)){
+      data <- read.table("Data/Protein/formatted_data/Q7_nonorm_formatted_impute50fil.csv", header=TRUE, sep=",", row.names=1, skip=0,
+                         nrows=-1, comment.char="", fill=TRUE, na.strings = "NA")
+    } else{
+      data <- read.table("Data/Protein/formatted_data/Q1-6_nonorm_formatted_impute50fil.csv", header=TRUE, sep=",", row.names=1, skip=0,
+                         nrows=-1, comment.char="", fill=TRUE, na.strings = "NA")  
+    }
+    output_dir <- "Data/Protein/"
+  } else{
+    print("Unknown dataset_id type !")
+    return
+  }
+  
   for(best_fsm in best_fsm_vec){
     data_sub <- data[gsub(".", "-", selected_features[[best_fsm]], fixed = TRUE),]
     print(dim(data_sub))
     print(sum(is.na(data_sub)))
-    subset_filename <- paste0("Data/RNA/", dataset_id, "_", best_fsm, 
+    subset_filename <- paste0(output_dir, dataset_id, "_", best_fsm, 
                               "_", min_iter_feature_presence,
-                              "_umi_counts.csv")
+                              ".csv")
     print(subset_filename)
     write.csv(data_sub, subset_filename)
+  }
+
+  #special case : get subset of features in t PREOPE Vs MET common in 28 iter similar across 4 FSMS
+  if(dataset_id == "GBMPlasmaEV_transcriptomic_PREOPEVsMET" &&
+     min_iter_feature_presence == 28){
+    #sncRNAs selected by only 4 of the FSMs
+    features <- setdiff(
+      Reduce(intersect, list(
+        selected_features[["ranger_impu_cor"]],
+        selected_features[["mrmr100"]],
+        selected_features[["t-test"]],
+        selected_features[["wilcoxontest"]]
+      )),
+      selected_features[["mrmr30"]]
+    )
+
+    data_sub <- data[gsub(".", "-", features, fixed = TRUE),]
+    print(dim(data_sub))
+    print(sum(is.na(data_sub)))
+    subset_filename <- paste0(output_dir, dataset_id, "_common_in_4FSM_", 
+                              min_iter_feature_presence,
+                              ".csv")
+    print(subset_filename)
+    write.csv(data_sub, subset_filename)
+    
   }
   
 }
 
-ds <- dataset_pipeline_arguments[[1]]
-dataset_id <- paste(ds$dataset_id, ds$classification_criteria, sep = "_")
-print(dataset_id) 
+arglist <- c(1,  #t PREOPE Vs MET
+             25  #p PREOPE Vs MET
+             ) 
 
-create_common_feature_plots_and_datasubsets(dataset_id = dataset_id,
-                                            best_fsm_vec = best_fsm_vec,
-                                            min_iter_feature_presence = 28)
-create_common_feature_plots_and_datasubsets(dataset_id = dataset_id,
-                                            best_fsm_vec = best_fsm_vec,
-                                            min_iter_feature_presence = 29)
+for(arg in arglist){
+  ds <- dataset_pipeline_arguments[[arg]]
+  dataset_id <- paste(ds$dataset_id, ds$classification_criteria, sep = "_")
+  print(dataset_id) 
+  
+  create_common_feature_plots_and_datasubsets(dataset_id = dataset_id,
+                                              best_fsm_vec = best_fsm_vec,
+                                              min_iter_feature_presence = 28)
+  create_common_feature_plots_and_datasubsets(dataset_id = dataset_id,
+                                              best_fsm_vec = best_fsm_vec,
+                                              min_iter_feature_presence = 29)
+}
+
+
