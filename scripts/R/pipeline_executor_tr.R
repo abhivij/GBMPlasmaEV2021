a258fa33062921d356f5b408433e86c52c3a2a44
abhivij <abhishek.kcvijayan@gmail.com>
2022-06-19 17:31:12 +1000

    var name correction

--- scripts/R/pipeline_executor_tr.R
+++ scripts/R/pipeline_executor_tr.R
@@ -4,11 +4,11 @@ source("scripts/R/dataset_pipeline_arguments_transcriptomic.R")
 args = commandArgs(trailingOnly = TRUE)
 if (length(args) > 1) {
   print(paste('Executing pipeline on dataset', args[2]))
-  dparg = dataset_pipeline_arguments[[strtoi(args[2])]]
+  dparg = dataset_pipeline_arguments_transcriptomic[[strtoi(args[2])]]
   do.call(execute_pipeline, dparg)
 } else {
   print('Executing pipeline on all datasets')
-  for (dparg in dataset_pipeline_arguments) {
+  for (dparg in dataset_pipeline_arguments_transcriptomic) {
     do.call(execute_pipeline, dparg)
   }
 }
