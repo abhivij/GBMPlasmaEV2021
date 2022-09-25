2ad69b9a7d0f644febb35d90cebd768a18a12f7d
abhivij <abhishek.kcvijayan@gmail.com>
2022-09-25 21:08:00 +1000

    call process new cohort

--- scripts/katana/process_protein_data_main.sh
+++ scripts/katana/process_protein_data_main.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 qsub -l select=1:ncpus=8:mem=124gb,walltime=24:00:00 -J 1-23 process_protein_data_main.pbs
+qsub -l select=1:ncpus=8:mem=124gb,walltime=48:00:00 process_protein_data_main_newcohort.pbs
