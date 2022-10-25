ab317c9afc68f9bba8b6a37792ad4c6bdc7859a6
abhivij <abhishek.kcvijayan@gmail.com>
2022-10-25 11:17:08 +1100

    generate bioreplicate and condition

--- scripts/R/proteomic/validation_format_data.R
+++ scripts/R/proteomic/validation_format_data.R
@@ -14,21 +14,23 @@ for(f in file_names){
   # data <- data_copy
   # length(unique(data$Replicate))
   
-  # data <- data %>%
-  #   mutate(BioReplicate = sub("4_Eclipse-OT_SH_|9_Eclipse-OT_SH_", "", Replicate)) %>%
-  #   mutate(BioReplicate = sub("-", "", BioReplicate, fixed = ""))
-  # 
-  # data <- data %>%
-  #   left_join(validation_metadata %>% select(sample_id, category_old_name), by = c("BioReplicate" = "sample_id")) %>%
-  #   mutate(Condition = case_when(is.na(category_old_name) ~ 'QC',
-  #                                TRUE ~ category_old_name)) %>%
-  #   select(-c(category_old_name))
+  data <- data %>%
+    mutate(BioReplicate = sub("4_Eclipse-OT_SH_|9_Eclipse-OT_SH_", "", Replicate)) %>%
+    mutate(BioReplicate = sub("-", "", BioReplicate, fixed = ""))
+
+  data <- data %>%
+    left_join(validation_metadata %>% select(sample_id, category_old_name), by = c("BioReplicate" = "sample_id")) %>%
+    mutate(Condition = case_when(is.na(category_old_name) ~ 'QC',
+                                 TRUE ~ category_old_name)) %>%
+    select(-c(category_old_name))
   
   # length(unique(data$BioReplicate))
   
   data <- data %>% rename("ProteinName" = "Protein", 
                           "PeptideSequence" = "Peptide", 
                           "FileName" = "Result.File.Locator")
+
+  
   # data <- data %>% filter(!grepl("precursor", Fragment.Ion))
   
   
